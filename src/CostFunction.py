adds cost function derivatives and corrects back_propagation method

src/CostFunction.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import numpy as np
 
+
 # TODO check this: https://stats.stackexchange.com/questions/154879/a-list-of-cost-functions-used-in-neural-networks
 #  -alongside-applications
 
@@ -11,20 +12,33 @@ class CostFunction(ABC):
     def calculate(self, value: np.ndarray, expected_value: np.ndarray) -> np.ndarray:
         pass
 
-
-class DistanceFunction(CostFunction):
-
-    def calculate(self, value: np.ndarray, expected_value: np.ndarray) -> np.ndarray:
-        return value - expected_value
+    @abstractmethod
+    def calculate_derivative(self, value: np.ndarray, expected_value: np.ndarray) -> np.ndarray:
+        pass
 
 
-class DistanceAbsFunction(CostFunction):
+class MeanAbsoluteError(CostFunction):
 
     def calculate(self, value: np.ndarray, expected_value: np.ndarray) -> np.ndarray:
         return np.abs(value - expected_value)
 
+    def calculate_derivative(self, value: np.ndarray, expected_value: np.ndarray) -> np.ndarray:
+        return np.vectorize(lambda x: -1 if x > 0 else 1)(value - expected_value)
 
-class DistanceQuadraticFunction(CostFunction):
+
+class MeanSquaredError(CostFunction):
 
     def calculate(self, value: np.ndarray, expected_value: np.ndarray) -> np.ndarray:
-        return (value - expected_value) ** 2
+        return np.power(value - expected_value, 2) / 2
+
+    def calculate_derivative(self, value: np.ndarray, expected_value: np.ndarray) -> np.ndarray:
+        return value - expected_value
+
+
+class CrossEntropy(CostFunction):
+
+    def calculate(self, value: np.ndarray, expected_value: np.ndarray) -> np.ndarray:
+        return - (expected_value * np.log(value) + (1 - expected_value) * np.log(1 - value))
+
+    def calculate_derivative(self, value: np.ndarray, expected_value: np.ndarray) -> np.ndarray:
+        return np.divide(value - expected_value, np.multiply(1 - value, value))

src/NeuronalNetwork.py
@@ -72,24 +72,19 @@ class NeuronalNetwork:
     def back_propagation(self, result, expected) -> Tuple[List[np.ndarray], List[np.ndarray]]:
         bias = []
         weight = []
-        activations = self.forward(result)
 
-        layer = self.hidden_layers[-1]
-        derivative = layer.activationFunction.calculate_derivative
-        z = layer.last_output
-        delta = self.cost_function.calculate(activations, expected) * derivative(z)
-
-        bias.append(delta)
-        weight.append(np.dot(delta, layer.last_input.T))
+        gradient = self.cost_function.calculate_derivative(result, expected)
 
         # FIXME check index out of the range
         for layer in self.hidden_layers[::-1]:
             z = layer.last_output
-            derivative = layer.activationFunction.calculate_derivative
-            sp = derivative(z)
-            delta = np.dot(layer.weight.T, delta) * sp
 
-            bias.append(delta)
-            weight.append(np.dot(delta, layer.last_input.T))
+            # element-wise multiplication
+            gradient = np.multiply(gradient, layer.activationFunction.calculate_derivative(z))
+
+            bias.append(gradient)
+            weight.append(np.dot(gradient, layer.last_input.T))
+
+            gradient = np.dot(layer.weight.T, gradient) 
 
         return bias, weight