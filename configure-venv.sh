c3c39e5826e0c937d817536409086c3ecdb67b87
Alejandro Anzola Avila <anzola95avila@gmail.com>
2020-02-09 22:21:16 -0500

    adds matplotlib to virtual environment init script

--- configure-venv.sh
+++ configure-venv.sh
@@ -4,4 +4,4 @@ python3 -m venv venv/
 # assumming that you're using zsh
 source venv/bin/activate
 python3 -m pip install -U pip
-python3 -m pip install numpy scipy
+python3 -m pip install numpy scipy matplotlib
