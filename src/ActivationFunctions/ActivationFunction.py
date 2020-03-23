Fixes Softmax gradient calculation

src/ActivationFunctions/ActivationFunction.py
@@ -9,6 +9,9 @@ import numpy as np
 
 class ActivationFunction(ABC):
 
+    def operation(self):
+        return np.multiply
+
     @abstractmethod
     def calculate(self, value: np.array) -> np.array:
         pass
@@ -56,10 +59,40 @@ class TanH(ActivationFunction):
 
 class SoftMax(ActivationFunction):
 
-    def calculate_gradient(self, value: np.array) -> np.array:
-        calculated_value = self.calculate(value)
-        return -np.outer(calculated_value, calculated_value) + np.diag(calculated_value.flatten())
+    def __init__(self):
+        self.last_output = None
+        self.last_input = None
 
-    def calculate(self, value: np.array) -> np.array:
-        e_value = np.exp(value - np.max(value))
-        return e_value / np.sum(e_value)
+    def operation(self):
+        return lambda x, y: np.einsum('ijk,ik->ij', x, y.T).T
+
+    def calculate_gradient(self, da: np.array) -> np.array:
+        # z, da shapes - (m, n)
+        z = self.last_input
+        m, n = z.T.shape
+        p = self.calculate(z.T)
+        # First we create for each example feature vector, it's outer product with itself
+        # ( p1^2  p1*p2  p1*p3 .... )
+        # ( p2*p1 p2^2   p2*p3 .... )
+        # ( ...                     )
+        tensor1 = np.einsum('ij,ik->ijk', p, p)  # (m, n, n)
+        # Second we need to create an (n,n) identity of the feature vector
+        # ( p1  0  0  ...  )
+        # ( 0   p2 0  ...  )
+        # ( ...            )
+        tensor2 = np.einsum('ij,jk->ijk', p, np.eye(n, n))  # (m, n, n)
+        # Then we need to subtract the first tensor from the second
+        # ( p1 - p1^2   -p1*p2   -p1*p3  ... )
+        # ( -p1*p2     p2 - p2^2   -p2*p3 ...)
+        # ( ...                              )
+        dSoftmax = tensor2 - tensor1
+        # Finally, we multiply the dSoftmax (da/dz) by da (dL/da) to get the gradient w.r.t. Z
+        # dz = np.einsum('ijk,ik->ij', dSoftmax, da.T)  # (m, n)
+        return dSoftmax
+
+    def calculate(self, z: np.array) -> np.array:
+        self.last_input = z
+        e = np.exp(z - np.max(z))
+        s = np.sum(e, axis=1, keepdims=True)
+        self.last_output = e / s
+        return self.last_output

src/Examples/RelationsExample.py
@@ -1,6 +1,8 @@
+from src.ActivationFunctions.ActivationFunction import SoftMax, Relu, TanH
 from src.BatchFunctions.BatchFunction import MiniBatch
 from src.CostFunctions.CostFunction import *
 from src.Examples.ExampleTemplate import ExampleTemplate
+from src.InitializationFunctions.InitializationFunction import He, Random
 from src.Networks.Layer.ClassicLayer import ClassicLayer, Sigmoid
 
 
@@ -32,10 +34,10 @@ class RelationExample(ExampleTemplate):
     def define_architecture(self):
         self.architecture = []
 
-        layer_1 = ClassicLayer(6)
-        layer_2 = ClassicLayer(8)
-        layer_3 = ClassicLayer(8)
-        layer_4 = ClassicLayer(2, Sigmoid())
+        layer_1 = ClassicLayer(6, Relu(), Random())
+        layer_2 = ClassicLayer(8, Relu(), Random())
+        layer_3 = ClassicLayer(8, Relu(), Random())
+        layer_4 = ClassicLayer(2, SoftMax())
 
         self.architecture.append(layer_1)
         self.architecture.append(layer_2)
@@ -47,7 +49,7 @@ class RelationExample(ExampleTemplate):
     def define_training_hyperparameters(self):
         self.learning_rate = 0.05
         self.iterations = 1_500
-        self.batch_function = MiniBatch(self.training_data, self.expected_output, 1)
+        self.batch_function = MiniBatch(self.training_data, self.expected_output, 5)
 
     def run_tests(self):
         r = self.neural_net.predict(np.array([1, 1, 0, 0, 0, 0]).reshape(-1, 1)).T

src/Networks/Layer/ClassicLayer.py
@@ -31,7 +31,7 @@ class ClassicLayer(Layer):
     def backward(self, gradient: np.array, learning_rate: float,
                  regularization_function: NormRegularizationFunction) -> np.array:
         dz = self.activationFunction.calculate_gradient(self.last_activation_output)
-        final_gradient = np.multiply(gradient, dz)
+        final_gradient = self.activationFunction.operation()(dz, gradient)
         bias_gradient = final_gradient + regularization_function.calculate_gradient_bias(self)
         weight_gradient = final_gradient @ self.last_input.T
         weight_gradient += regularization_function.calculate_gradient_weights(self)

src/Networks/Layer/Convolution/MaxPoolingLayer.py
@@ -30,7 +30,7 @@ class MaxPoolingLayer(Layer):
         for im_region, i, j in self.__iterate_regions__(x_input):
             output[i, j] = np.amax(im_region, axis=(0, 1))
         self.last_output = output
-        return self.last_output
+        return self.last_output.flatten()
 
     def backward(self, gradient: np.array, learning_rate: float,
                  regularization_function: NormRegularizationFunction) -> np.array:

src/Networks/Layer/SoftMaxLayer.py
@@ -36,36 +36,7 @@ class SoftMaxLayer(Layer):
 
     def backward(self, gradient: np.array, learning_rate: float,
                  regularization_function: NormRegularizationFunction) -> np.array:
-
-        for i, x_gradient in enumerate(gradient):
-            # e^totals
-            t_exp = np.exp(self.last_output)
-
-            # Sum of all e^totals
-            s = np.sum(t_exp)
-
-            # Gradients of out[i] against totals
-            d_out_d_t = -t_exp[i] * t_exp / (s ** 2)
-            d_out_d_t[i] = t_exp[i] * (s - t_exp[i]) / (s ** 2)
-
-            # Gradients of totals against weights/biases/input
-            d_t_d_w = self.last_input
-            d_t_d_b = 1
-            d_t_d_inputs = self.weight
-
-            # Gradients of loss against totals
-            d_L_d_t = x_gradient * d_out_d_t
-
-            # Gradients of loss against weights/biases/input
-            d_L_d_w = d_L_d_t @ d_t_d_w.reshape(1, -1)
-            d_L_d_b = d_L_d_t * d_t_d_b
-            d_L_d_inputs = d_t_d_inputs.T @ d_L_d_t
-
-            # Update weights / biases
-            self.update_weight(learning_rate, d_L_d_w)
-            self.update_bias(learning_rate, d_L_d_b)
-
-            return d_L_d_inputs.reshape(self.last_input_shape)
+        pass
 
     def update_weight(self, learning_rate: float, grads: np.array):
         assert self.weight.shape == grads.shape